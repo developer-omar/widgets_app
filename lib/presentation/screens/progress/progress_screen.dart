a35ad131c10b78ce7f991c9422cd6341006795f0
Omar Enrique <oquispe1984@gmail.com>
2024-06-25 15:38:33 -0400

    new advances

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -7,6 +7,70 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Progress indicators'),
+      ),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(height: 30,),
+          Text('Circular progress indicator'),
+          SizedBox(height: 10,),
+          CircularProgressIndicator(
+            strokeWidth: 2,
+            backgroundColor: Colors.black45,
+          ),
+          SizedBox(height: 20),
+          Text('Circular y Linear Controlado'),
+          SizedBox(height: 10),
+          _ControlledProgressIndicator()
+        ],
+      ),
+    );
+  }
+}
+
+class _ControlledProgressIndicator extends StatelessWidget {
+  const _ControlledProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder:(context, snapshot) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: 0.5,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: LinearProgressIndicator(
+                  value: 0.5,
+                )
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }
